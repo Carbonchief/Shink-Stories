2f3e4a2c429e8f689a0582e46eb8fb795adf5eb2
Luan van der Walt <luan@prioritybit.co.za>
2026-03-29 22:56:18 +0200

    Updated

---
+++ Shink.Mobile/Platforms/iOS/Program.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace Shink.Mobile;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
