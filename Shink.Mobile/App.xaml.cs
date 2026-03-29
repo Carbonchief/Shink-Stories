b332d9032c6453df5fe91209dbee036baaecbc0d
Luan van der Walt <luan@prioritybit.co.za>
2026-03-29 22:56:18 +0200

    Updated

---
+++ Shink.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+namespace Shink.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Shink.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace Shink.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
