c39abab0f0f675f344b417913a2afa8081dfa61a
Luan van der Walt <luan@prioritybit.co.za>
2026-05-05 05:52:19 +0200

    Fix transparent admin edit form backgrounds

---
+++ Shink.Tests/AdminDialogSourceTests.cs
@@ -0,0 +1,34 @@
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shink.Tests;
+
+[TestClass]
+public class AdminDialogSourceTests
+{
+    [TestMethod]
+    public void EditorDialogsUseSolidAdminDialogClass()
+    {
+        var markup = File.ReadAllText(GetRepoPath("Shink", "Components", "Pages", "Admin.razor"));
+        var globalCss = File.ReadAllText(GetRepoPath("Shink", "wwwroot", "app.css"));
+
+        StringAssert.Contains(markup, "<MudDialog Visible=\"IsNewStoryDialogOpen\"\n                       VisibleChanged=\"OnNewStoryDialogVisibleChanged\"\n                       Class=\"admin-solid-dialog\"\n                       ContentClass=\"admin-solid-dialog-content\"\n                       Options=\"EditorDialogOptions\">");
+        StringAssert.Contains(markup, "<MudDialog Visible=\"IsStoryDialogOpen\"\n                       VisibleChanged=\"OnStoryDialogVisibleChanged\"\n                       Class=\"admin-solid-dialog\"\n                       ContentClass=\"admin-solid-dialog-content\"\n                       Options=\"EditorDialogOptions\">");
+        StringAssert.Contains(markup, "<MudDialog Visible=\"IsPlaylistDialogOpen\"\n                       VisibleChanged=\"OnPlaylistDialogVisibleChanged\"\n                       Class=\"admin-solid-dialog\"\n                       ContentClass=\"admin-solid-dialog-content\"\n                       Options=\"EditorDialogOptions\">");
+        StringAssert.Contains(globalCss, ".mud-dialog.admin-solid-dialog");
+        StringAssert.Contains(globalCss, ".mud-dialog.admin-solid-dialog .mud-input-outlined");
+    }
+
+    private static string GetRepoPath(params string[] segments)
+    {
+        var parts = new[]
+        {
+            Path.GetDirectoryName(GetSourceFilePath())!,
+            ".."
+        }.Concat(segments).ToArray();
+
+        return Path.GetFullPath(Path.Combine(parts));
+    }
+
+    private static string GetSourceFilePath([CallerFilePath] string path = "") => path;
+}
